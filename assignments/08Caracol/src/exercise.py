5dcd22f09284efb38efd6122c765b4ecb608ad3e
ivaragon <88863106+ivaragon@users.noreply.github.com>
2021-08-25 20:49:18 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
+    min=float(input('Dame los minutos: '))
+    cm=min * 34.2
+    print('Centímentros recorridos:','{0:.1f}'.format(cm))
 
-
-    print("Centímentros recorridos:",cm)
-
+    
 
 if __name__ == '__main__':
     main()
